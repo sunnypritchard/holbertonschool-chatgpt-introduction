d508f0abfc066991aa2926e1f8cab2cf7a7a2222
Sunny Pritchard <pritchardsunny@gmail.com>
2025-09-01 10:27:23 +0200

    fix(task 1_debugging): start loop from index [1] of the argument list to exclude the script name

--- print_arguments.py
+++ print_arguments.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Print all arguments passed to the script except the script name
-# Fix loop start to index [1] excluding the script name
 for i in range(1, len(sys.argv)):
     print(sys.argv[i])
